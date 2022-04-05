915a8fdfffb8e3f93e2162a0efa0dcb8156e494f
dyllanwli <messsking@gmail.com>
2022-04-04 20:06:09 -0500

    init

---
+++ install.sh
@@ -0,0 +1,4 @@
+pip install https://data.dgl.ai/wheels/dgl_cu115-0.8.0-cp38-cp38-manylinux1_x86_64.whl
+pip install -U pip setuptools wheel
+pip install -U 'spacy[cuda114]'
+python -m spacy download en_core_web_sm
