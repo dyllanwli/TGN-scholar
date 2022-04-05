b276e5d679c6c0aaf67294afae59daf1973e2171
dyllanwli <messsking@gmail.com>
2022-04-05 14:58:06 -0500

    update

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ pip install https://data.dgl.ai/wheels/dgl_cu115-0.8.0-cp38-cp38-manylinux1_x86_
 pip install -U pip setuptools wheel
 pip install -U 'spacy[cuda114]'
 python -m spacy download en_core_web_sm
+pip install pyvis
